4d93d18737deebffd9694c29782b9227b07a964c
Karlo <karlo@hrva.cc>
2023-11-27 18:38:46 +0100

    INIT

---
+++ src/test/java/cc/hrva/internalservices/InternalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.hrva.internalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
